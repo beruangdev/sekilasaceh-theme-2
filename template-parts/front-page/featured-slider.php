25e1fbceddf2fc437962088b026b9a105ecb7acd
beruangdev <beruangdev@gmail.com>
2022-12-11 21:07:47 +0700

    tamah

--- template-parts/front-page/featured-slider.php
+++ template-parts/front-page/featured-slider.php
@@ -102,10 +102,10 @@ $query_featured = new WP_Query($args);
 <script>
     const swiper = new Swiper('.swiper', {
         loop: true,
-        // autoplay: {
-        //     delay: 2500,
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
         // If we need pagination
         // pagination: {
         //     el: '.swiper-pagination',
